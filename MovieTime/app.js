f8b4e984909e7a841559b7f551e758f6c75ae006
AdilZaib <58344410+AdilZaib@users.noreply.github.com>
2020-06-21 23:03:53 +0500

    Uploaded my MovieTime Website

---
+++ MovieTime/app.js
@@ -0,0 +1,63 @@
+function show()
+{
+    var x=document.querySelector(".hide");
+    if(x.classList.contains("hide"))
+    {
+        x.classList.toggle("show");
+    }
+}
+
+// CSS MODEL SCRIPT
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+
+  // SIMILAR TAB 
+  
+  function showSimilar(n) 
+  {
+
+      document.querySelector("#s"+n).style.display="block";
+  }
+  function hideSimilar(n) 
+  {
+      document.querySelector("#s"+n).style.display="none";
+  }
+
